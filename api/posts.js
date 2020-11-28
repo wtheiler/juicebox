ab24534f0790d8a7e2b2849f9a136750f07af80e
Will Theiler <will@Wills-MacBook-Air.local>
2020-11-27 22:58:05 -0500

    working part 3

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
+
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData = {};
+
+    // only send the tags if there are some to send
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try {
+        // add authorId, title, content to postData object
+        if (authorId) {
+            postData.id = authorId
+        }
+
+        if (title) {
+            postData.title = title
+        }
+
+        if (content) {
+            postData.content = content
+        }
+
+
+        console.log(postData)
+        const post = await createPost(postData);
+
+        // this will create the post and the tags for us
+        // if the post comes back, res.send({ post });
+        if (post) {
+            res.send({ post })
+        } else {
+            next({ name: "error postsRouter", message: "There was an error retrieving the Post." })
+        }
+        // otherwise, next an appropriate error object 
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
